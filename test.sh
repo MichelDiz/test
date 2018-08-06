8565eb7d98173e277b2f271ff5e0fe4ccde0ce5e
Michel Conrado <michelconrado@outlook.com>
2018-08-06 14:32:08 -0300

    3
    3

--- test.sh
+++ test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-while [ ! -f ./done.lock ];
-do;
-    echo 'Waiting for it...';
+while [ ! -f ./done.lock ]
+do
+    echo 'Waiting for it...'
     sleep 10;
-done;
-echo './start.sh';
+done
+echo './start.sh'
+echo './start.sh'
+echo './start.sh'
+echo './start.sh'
+echo './start.sh'
 # ./start.sh
